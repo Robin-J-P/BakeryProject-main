2caf56119a90e1383fa1a2db9b4ba3103f89c249
Robin-J-P <panicker.robin2@gmail.com>
2022-12-07 20:14:46 -0500

    Commit

--- BakeryProject-main/src/components/Footer/index.js
+++ BakeryProject-main/src/components/Footer/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import "./styles.scss";
+import React from 'react';
+import './styles.scss';
 
 const Footer = (props) => {
   return (
-    <footer className="footer">
-      <div className="wrap">© Thattikootu teams Pvt LTD 2022</div>
+    <footer className='footer'>
+      <div className='wrap'>© 3D Bakery Pvt LTD 2022</div>
     </footer>
   );
 };
